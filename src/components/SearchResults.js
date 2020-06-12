d7fb4d305135c605d4089bced50d28817b2415c6
Levi Plant <levijplant@gmail.com>
2020-06-11 21:48:07 -0700

    class done for 06/11/2020

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,10 +1,31 @@
 import React from 'react';
 
-const searchResults = ({ results }) => {
+import Card from './Card';
+import './SearchResults.css';
+
+const searchResults = ({ 
+    results,
+    addCardToDeck,
+    removeCardFromDeck
+}) => {
     return(
         <div id="results">
-            <h3>Here's what we found ({ results.length } results):</h3>
-        </div>    );
+            <h3>
+                Here's what we found ({ results.length } results):
+            </h3>
+            <div className="CardList"> 
+                {
+                    results.map(result => (
+                        <Card 
+                            key={ result.id }
+                            addCardToDeck={ addCardToDeck }
+                            removeCardFromDeck={ removeCardFromDeck }
+                            { ...result } />
+                    ))
+                }
+            </div>
+        </div>
+    );
 };
 
 export default searchResults;
